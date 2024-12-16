a3ba09e88816ac9c6637a6dc0a5f83ab04b6f6b5
milan.soap123 <milan.seid123@gmail.com>
2024-12-16 14:30:36 +0100

    Instanciating toll service

---
+++ toll-service/src/main/java/com/example/toll_service/TollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.toll_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollServiceApplication.class, args);
+	}
+
+}
---
+++ toll-service/src/test/java/com/example/toll_service/TollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toll_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
